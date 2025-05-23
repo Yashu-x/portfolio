d0451f5e95f5367a27d491a22228a1b41355d89b
Yashu-x <n.thashenu@gmail.com>
2025-05-23 14:00:19 +0530

    add k8s and helm

--- components/Primary.tsx
+++ components/Primary.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Spotlight from './Ui/Spotlight'
+import { TextGenerateEffect } from './Ui/TextGenerateEffect'
+
 
 const Primary = () => {
   return (
@@ -11,6 +13,28 @@ const Primary = () => {
             <Spotlight className=" top-28 left-80 h-[80vh] w-[50vw] " fill='blue' />
 
         </div>
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.09] bg-grid-black/[0.5]  flex items-center justify-center absolute top-0 left-0">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+      </div>
+
+    </div>
+    <div className="flex justify-center relative my-4 z-10">
+      <div className='max-w-[150vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+        <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Thashenu&apos;s portfolio </h2>
+       
+        <TextGenerateEffect 
+  className="text-center text-2xl md:text-3xl font-bold"
+  words={`Hi, I'm Thashenu Kularathna, an ICT undergraduate and  Traning System Engineer (DevOps) with a strong passion for DevOps and cloud computing. I specialize in containerization, automation, and infrastructure management, working with technologies like Linux, Docker, Kubernetes, Terraform, Snyk, and CI/CD pipelines.
+
+My technical expertise includes Java, C, Go, Bash, and JavaScript, along with tools such as Git, Jira, VS Code, Docker, Kubernetes, Terraform, Prometheus, Grafana, React, Ansible, and NGINX. I also work with Linux-based systems, MySQL, MongoDB, Jenkins, GitHub Actions, and cloud platforms like AWS, GCP, and Azure.
+
+I&apos;m always eager to explore new technologies and improve system performance through automation. Let&apos;s connect and innovate together! 🚀`}
+  filter={true}
+/>
+
+        </div>
+    </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Thashenu | Portfolio",
@@ -23,12 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
   );
 }
